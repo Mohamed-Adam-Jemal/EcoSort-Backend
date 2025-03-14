b4cab2fc8bedf5226beb17904ec67017f5db0322
Adam J. <mohamed.adam.jemal@gmail.com>
2025-03-14 09:37:30 +0000

    waste urls edit

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,18 +7,18 @@ from .views import (
 
 urlpatterns = [
     # User URLs
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail'),
+    path('users/', user_list, name='users-list'),
+    path('users/<int:pk>/', user_detail, name='users-detail'),
 
     # WasteBot URLs
-    path('wastebots/', wastebot_list, name='wastebot-list'),
+    path('wastebots/', wastebot_list, name='wastebots-list'),
     path('wastebots/<int:pk>/', wastebot_detail, name='wastebot-detail'),
 
     # SmartBin URLs
-    path('smartbins/', smartbin_list, name='smartbin-list'),
+    path('smartbins/', smartbin_list, name='smartbins-list'),
     path('smartbins/<int:pk>/', smartbin_detail, name='smartbin-detail'),
 
     # Waste URLs
-    path('waste/', waste_list, name='waste-list'),
-    path('waste/<int:pk>/', waste_detail, name='waste-list'),
+    path('wastes/', waste_list, name='wastes-list'),
+    path('wastes/<int:pk>/', waste_detail, name='waste-detail'),
 ]
